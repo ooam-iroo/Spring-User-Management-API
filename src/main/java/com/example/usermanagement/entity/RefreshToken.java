72beb0a6982dfbf07bc25aa41fe8e040d99516d6
amir <amyrmdi99@gmail.com>
2026-08-06 16:43:43 +0330

    feat: update refresh token entity

--- src/main/java/com/example/usermanagement/entity/RefreshToken.java
+++ src/main/java/com/example/usermanagement/entity/RefreshToken.java
@@ -15,11 +15,20 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class RefreshToken extends BaseEntity {
 
+    @Column(
+            nullable = false,
+            unique = true,
+            length = 500
+    )
     private String token;
 
+    @Column(nullable = false)
     private LocalDateTime expiryDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(
+            name = "user_id",
+            nullable = false
+    )
     private User user;
 }
