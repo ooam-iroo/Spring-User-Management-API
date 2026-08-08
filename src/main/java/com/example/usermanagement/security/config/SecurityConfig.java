edce387e7962bc00065b1e87885bace5140f051d
amir <amyrmdi99@gmail.com>
2026-08-08 16:04:10 +0330

    feat: configure authentication manager

--- src/main/java/com/example/usermanagement/security/config/SecurityConfig.java
+++ src/main/java/com/example/usermanagement/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.usermanagement.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,5 +41,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+
+        return configuration.getAuthenticationManager();
+
     }
 }
