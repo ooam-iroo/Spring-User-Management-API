785c40a2508c37b09a1404a5a2352214b012a20e
amir <amyrmdi99@gmail.com>
2026-08-15 17:01:36 +0330

    feat(user): add current user endpoint

--- src/main/java/com/example/usermanagement/SpringUserManagementApiApplication.java
+++ src/main/java/com/example/usermanagement/SpringUserManagementApiApplication.java
@@ -3,9 +3,11 @@ package com.example.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMethodSecurity
 public class SpringUserManagementApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/usermanagement/controller/user/UserController.java
@@ -0,0 +1,18 @@
+package com.example.usermanagement.controller.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/me")
+    public String getCurrentUser(
+            Authentication authentication
+    ){
+        return authentication.getName();
+    }
+}
